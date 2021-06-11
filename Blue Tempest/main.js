86baa61ddbdf9f63efad41651bd07e3b84c5ef6e
Ariel Elege <arielelege@gmail.com>
2021-06-11 15:24:27 +0100

    changed hero bg, made mobile nav

--- Blue Tempest/main.js
+++ Blue Tempest/main.js
@@ -1,62 +1,39 @@
-let burger = document.getElementsByClassName("bnav-burger")[0],
-     nav = document.getElementsByClassName("bnav")[0],
-     navList = document.getElementsByClassName("bnav__list")[0],
-     navListItems = document.querySelectorAll(".bnav__list-items"),
-     navLinks = document.querySelectorAll(".bnav-links"),
-     activeLink = document.getElementsByClassName("active")[0],
-     clicked = false;
+let burger = document.querySelector(".bnav-burger") 
+let nav = document.querySelector(".bnav")
+let navList = document.querySelector(".bnav__list")
+let navListItems = Array.from(document.querySelectorAll(".bnav__list-items"))
+let navLinks = Array.from(document.querySelectorAll(".bnav-links"))
+let activeLink = document.querySelector(".active")
 
 // for the header color shifting
-let header = document.getElementsByTagName("header")[0],
-     sHeader = document.getElementsByClassName("site-header__text")[0],
-     story = document.querySelector("section div h1");
+let header = document.querySelector("header")
+let sHeader = document.querySelector(".site-header__text")
+let story = document.querySelector("section div h1");
 
 // for the scroll events
-let body = document.body,
-     docElem = document.documentElement,
-     scrollPosition = Math.round(window.scrollY),
-     docHeight = Math.max(
+let body = document.body
+let docElem = document.documentElement
+let scrollPosition = Math.round(window.scrollY)
+let docHeight = Math.max(
      body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight
- );
+);
+
+
+let toggleNav = (event) => {
+	event.preventDefault
+	nav.classList.contains('bnav-open') ?
+		burger.innerHTML = 'Menu':
+			burger.innerHTML = 'X'
+	burger.classList.toggle("bnav-burger-open");
+	nav.classList.toggle("bnav-open");
+}
+
+let headerColorShifter = () => {
+	scrollPosition = body.scrollTop || docElem.scrollTop;
+		scrollPosition > 40 ?
+			header.classList.add("darker"):
+				header.classList.remove("darker")
+}
 
 burger.addEventListener("click", toggleNav);
 window.addEventListener("scroll", headerColorShifter);
-
-function toggleNav () {
-     if ( !clicked ) {
-          burger.classList.add("bnav-burger-open");
-          nav.classList.add("bnav-open");
-          navList.classList.add("bnav-open__list");
-
-          Array.from(navListItems).map(element => {
-               element.classList.add("bnav-open__list-items");
-          });
-          Array.from(navLinks).map(element => {
-               element.classList.add("bnav-open-links");
-          });
-
-          clicked = true;
-
-     } else {
-          burger.classList.remove("bnav-burger-open");
-          nav.classList.remove("bnav-open");
-          navList.classList.remove("bnav-open__list");
-
-          Array.from(navListItems).map(element => {
-               element.classList.remove("bnav-open__list-items");
-
-          });
-          Array.from(navLinks).map(element => {
-               element.classList.remove("bnav-open-links");
-          });
-          
-          clicked = false;
-
-     }
-}
-function headerColorShifter () {
-     scrollPosition = body.scrollTop || docElem.scrollTop;
-          scrollPosition > 40 ?
-               header.classList.add("darker"):
-                    header.classList.remove("darker")
-}
