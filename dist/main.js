8c53d1685b161f15dc47b5f0591a4db40f4f8e39
Ariel Elege <arielelege@gmail.com>
2021-06-13 21:46:37 +0100

    made footer, made remove nav function

--- dist/main.js
+++ dist/main.js
@@ -4,12 +4,12 @@ let navList = document.querySelector(".bnav__list")
 let navListItems = Array.from(document.querySelectorAll(".bnav__list-items"))
 let navLinks = Array.from(document.querySelectorAll(".bnav-links"))
 let activeLink = document.querySelector(".active")
-
+let mainTag = document.querySelector("main")
+let footerTag = mainTag.nextElementSibling
 // for the header color shifting
 let header = document.querySelector("header")
 let sHeader = document.querySelector(".site-header__text")
-let story = document.querySelector("section div h1");
-
+let story = document.querySelector("section div h1")
 // for the scroll events
 let body = document.body
 let docElem = document.documentElement
@@ -17,15 +17,27 @@ let scrollPosition = Math.round(window.scrollY)
 let docHeight = Math.max(
      body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight
 );
+let contactInput = Array.from(document.querySelectorAll('.contact-form input'))
 
 
+let removeNav = () => {
+	if ( nav.classList.contains('bnav-open') ) {
+		burger.innerText = 'Menu'
+		burger.classList.remove("bnav-burger-open")
+		nav.classList.remove("bnav-open")
+	}
+}
 let toggleNav = (event) => {
 	event.preventDefault()
-	nav.classList.contains('bnav-open') ?
-		burger.innerHTML = 'Menu':
-			burger.innerText = 'X'
-	burger.classList.toggle("bnav-burger-open");
-	nav.classList.toggle("bnav-open");
+	if ( !nav.classList.contains('bnav-open') ) {
+		burger.innerText = 'X'
+		burger.classList.add("bnav-burger-open");
+		nav.classList.add("bnav-open");
+	} else {
+		burger.innerText = 'Menu'
+		burger.classList.remove("bnav-burger-open");
+		nav.classList.remove("bnav-open");
+	}
 }
 
 let headerColorShifter = () => {
@@ -36,4 +48,19 @@ let headerColorShifter = () => {
 }
 
 burger.addEventListener("click", toggleNav);
+mainTag.addEventListener("click", removeNav);
+footerTag.addEventListener("click", removeNav);
 window.addEventListener("scroll", headerColorShifter);
+
+contactInput.forEach(input => {
+	input.addEventListener('focus', () => {
+	  let focusBorder = setTimeout(() => input.classList.add('focus'), 700)
+	  input.addEventListener('blur', () => {
+		 input.classList.remove('focus')
+		 clearTimeout(focusBorder)
+	  })
+	})
+	input.addEventListener('change', () => (e) => {
+	  input.value = e.target.value
+	})
+ })
